3818486b435af114d01c9a99ec0621dd00742d62
S <ap311036@gmail.com>
2018-12-03 21:41:09 +0800

    Updates

--- precache-manifest.274513a5f2dfc15412f8e22b7e8e9cf2.js
+++ precache-manifest.274513a5f2dfc15412f8e22b7e8e9cf2.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
+    "revision": "f591acac8845ee729dc8c09689c6e986",
+    "url": "/react_search/static/media/iconlion.f591acac.svg"
+  },
+  {
     "revision": "b4f3cf6d1b399f96a7396431e88528f5",
     "url": "/react_search/static/media/iconlion.b4f3cf6d.woff"
   },
   {
     "revision": "a4f3a8217908358286681c3ee3f4ae5b",
     "url": "/react_search/static/media/iconlion.a4f3a821.ttf"
-  },
-  {
-    "revision": "72d1d9ecf19a4941657b7f90fbfd5036",
-    "url": "/react_search/static/media/iconlion.72d1d9ec.svg"
   },
   {
     "revision": "068761fb3cf23262b39080c501859dd3",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/react_search/static/js/runtime~main.js"
   },
   {
-    "revision": "2321621802f662fbe726",
+    "revision": "07227657cf79ec6384bf",
     "url": "/react_search/static/js/main.chunk.js"
   },
   {
-    "revision": "f27e5de2995b1141a8a3",
+    "revision": "de5a76ba34602730546b",
     "url": "/react_search/static/js/1.chunk.js"
   },
   {
-    "revision": "2321621802f662fbe726",
+    "revision": "07227657cf79ec6384bf",
     "url": "/react_search/static/css/main.chunk.css"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react_search/precache-manifest.d3ee59e12b36a8163dc55243fbe8ae79.js"
+  "/react_search/precache-manifest.274513a5f2dfc15412f8e22b7e8e9cf2.js"
 );
 
 workbox.clientsClaim();
